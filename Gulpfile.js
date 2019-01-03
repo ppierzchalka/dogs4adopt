fb432e0b8ccf788314ae7f00fae8725d89cb0272
ppierzchalka <przemekpierzchalka@gmail.com>
2019-01-04 00:17:01 +0100

    boilerplate ready

---
+++ Gulpfile.js
@@ -0,0 +1,40 @@
+var gulp = require("gulp"),
+    sass = require("gulp-sass"),
+    postcss = require("gulp-postcss"),
+    autoprefixer = require("autoprefixer"),
+    cssnano = require("cssnano"),
+    sourcemaps = require("gulp-sourcemaps");
+
+// Put this after including our dependencies
+var paths = {
+    styles: {
+        src: "sass/**/*.scss",
+        dest: "public/css"
+    }
+};
+
+function style() {
+    return (
+        gulp
+            .src(paths.styles.src)
+            // Initialize sourcemaps before compilation starts
+            .pipe(sourcemaps.init())
+            .pipe(sass())
+            .on("error", sass.logError)
+            // Use postcss with autoprefixer and compress the compiled file using cssnano
+            .pipe(postcss([autoprefixer(), cssnano()]))
+            // Now add/write the sourcemaps
+            .pipe(sourcemaps.write())
+            .pipe(gulp.dest(paths.styles.dest))
+    );
+}
+exports.style = style;
+
+
+function watch() {
+style();
+
+gulp.watch(paths.styles.src, style);
+}
+
+exports.watch = watch
--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const fs = require('fs');
 const axios = require('axios');
 const express = require('express');
+var path = require("path");
 const hbs = require('hbs');
 
 
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(express.static(path.join(__dirname, '/public')));
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>');
+  res.render('index.hbs', {
+    pageTitle: 'Websited NodeJS Website',
+    bodyContent: 'Hi, and welcome to my newly created nodejs webpage'
+  });
 });
 
 app.listen(port, () => {console.log(`Server is up on port ${port}`)});
