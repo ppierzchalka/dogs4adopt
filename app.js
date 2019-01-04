431ccdcdd5ae5d5085cc80d35d9c7e685b5073ad
ppierzchalka <przemekpierzchalka@gmail.com>
2019-01-04 16:25:13 +0100

    working dynamic templates

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 const axios = require('axios');
 const express = require('express');
-var path = require("path");
+const path = require("path");
 const hbs = require('hbs');
+const src = 'https://api.myjson.com/bins/1b1nn8';
 
 
 const app = express();
@@ -12,11 +13,34 @@ hbs.registerPartials(__dirname + '/views/partials');
 
 app.set('view engine', 'hbs');
 
+var menuPaths = [{title: 'Home', path: '/'}, {title: 'About', path: '/about'}];
+
 app.get('/', (req, res) => {
   res.render('index.hbs', {
     pageTitle: 'Websited NodeJS Website',
-    bodyContent: 'Hi, and welcome to my newly created nodejs webpage'
+    pathToRender : 'homepage',
+    menu: menuPaths
   });
 });
+
+app.get('/about', (req, res) => {
+  res.render('index.hbs', {
+    pageTitle: 'About page',
+    pathToRender: 'about',
+    menu: menuPaths
+  });
+});
+
+axios.get(src)
+.then(res => res.data.posts.forEach(post => {
+  menuPaths.push({title: `${post.title}`, path: `/${post.path}`});
+  app.get(`/${post.path}`, (req, res) => {
+    res.render('post.hbs', {
+      pageTitle: post.title,
+      content: post.content,
+      menu: menuPaths
+    })
+  })
+}));
 
 app.listen(port, () => {console.log(`Server is up on port ${port}`)});
