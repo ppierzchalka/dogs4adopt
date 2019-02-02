00e627d8aeec2332707ee7b9cc357cd450051797
ppierzchalka <przemekpierzchalka@gmail.com>
2019-02-02 18:09:18 +0100

    added map page

--- app.js
+++ app.js
@@ -35,6 +35,19 @@ hbs.registerHelper('displayDogs', function(dog) {
   )
 });
 
+// hbs.registerHelper('listdogs', function(dog) {
+//   return new hbs.SafeString(
+//     `<a href=${this.link} target="_blank">
+//     <h3 class="name">${this.name}</h3>
+//     <div class="img-crop">
+//     <img class="dog-img" src=${this.image} alt=${this.name} onerror="this.onerror=null;this.src='/images/noimage.png';"/>
+//     </div>
+//     <p class="location">Lokalizacja: ${this.location}</p>
+//     <button class="view">Zobacz</button>
+//     </a>`
+//   )
+// });
+
 function generateShelterPages(list) {
   list.forEach(shelter => {
     menuShelters.push({title: shelter, path: `/${encodeURIComponent(shelter)}`})
--- public/js/script.js
+++ public/js/script.js
@@ -15,8 +15,23 @@ $(document).ready(function() {
   const input = document.querySelector('#filter-input');
   const list = [...document.querySelectorAll('.filterable')];
   const listParent = document.querySelector('#filter-list');
+  const mapElem = document.querySelector('#map');
 
-  input.addEventListener('input', filterShelters);
+  if (input) {
+    input.addEventListener('input', filterShelters);
+  }
+
+  if (mapElem) {
+    var map = new google.maps.Map(mapElem, {
+      center: {
+        lat: 51.919437,
+        lng: 19.145136
+      },
+      zoom: 7,
+      disableDefaultUI: true,
+      styles: styles
+    });
+  }
 
   function filterShelters(e) {
     const value = e.target.value;
