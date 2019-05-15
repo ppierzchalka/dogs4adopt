dfbe0112f16065aed35d7237b48faff4fe8c54b3
ppierzchalka <przemekpierzchalka@gmail.com>
2019-05-15 21:03:27 +0200

    frontend removed

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
-const url = ''; // Specify Your db connection here
+const url = 'mongodb://mo1064_dogs4dopt:5Onw0yVtY3S7hwFqqdhU@91.185.191.145:27017/mo1064_dogs4dopt'; // Specify Your db connection here
 
 mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true
 })
+
+""
