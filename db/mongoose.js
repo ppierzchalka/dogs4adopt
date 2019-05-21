c9f70abf9bc9107ab27c48c1edbe120a5decfaa2
ppierzchalka <przemekpierzchalka@gmail.com>
2019-05-21 19:17:03 +0200

    merge backend-only to master

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb://mo1064_dogs4dopt:5Onw0yVtY3S7hwFqqdhU@91.185.191.145:27017/mo1064_dogs4dopt'; // Specify Your db connection here
+const url = ''; // Specify Your db connection here
 
 mongoose.connect(url, {
         useNewUrlParser: true,
