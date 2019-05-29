6acb7bf7c1afc7ba8746c2e7bc8168e920ac246f
ppierzchalka <przemekpierzchalka@gmail.com>
2019-05-29 22:05:50 +0200

    full pagination for pages: all, shelter pages, search results. added router to get shelter list

--- README.md
+++ README.md
@@ -14,19 +14,40 @@ This NodeJS app scrapes data from animal shelters in Poland and serves this data
 
 This app has api feature, api routes are listed below:
 
-- `/api/all` gets all dogs from database
-- `/api/shelter/[shelter]` gets dogs from certain `shelter`, currently working shelters are:
-  - `lodz` (Łódź)
-  - `dgorna` (Dłużyna Górna)
-- `/api/id/[id]` gets certain dog by it's `id`
-- `/api/name/[name]` searches dogs by `name`, or part of it
+- `/api/all` gets all dogs from database, returns array of dogs if no parameters were provided, optional parameters:
+  - limit - to limit how many dogs display per page,
+  - page - to display page by given number,
+  - example: `/api/all?limit=5&page=15`
+- `/api/shelterNames` - displays list of objects with shelters data, each object includes:
+  - `location` - string with full name of certain sheter,
+  - `dataLocation` - string which can be used to find dogs from certain shelter in path below 
+- `/api/shelter/[dataLocation]` gets dogs from certain shelter, get dataLocation values from previous path, optional parameters:
+  - limit - to limit how many dogs display per page,
+  - page - to display page by given number,
+  - example: `/api/shelter/lodz?limit=5&page=15`
+- `/api/id/[id]` returns certain dog searched by it's `id`
+- `/api/name/[name]` searches dogs by `name`, or part of it, returns array of dogs if no parameters were provided, optional parameters:
+  - limit - to limit how many dogs display per page,
+  - page - to display page by given number,
+  - example: `/api/name/burek?limit=5&page=15`
 - `/api/random/[x]` returns `x` random dogs
 
+## Optional parameters
+
+If both optional parameters are provided (only one parameter won't work), object is returned instead of array. Structure of object include:
+- `docs` - actual array of results,
+- `totalDocs` - number of results,
+- `limit` - limiter provided in parameter (indicates how many documents are on single page),
+- `hasPrevPage` - boolean, indicates is there a previous page,
+- `hasNextPage` - boolean, indicates is there a next page,
+- `totalPages` - number of all pages within given query
+- `pagingCounter` - number of first of item on page requested within given query,
+- `prevPage` - number of previous page,
+- `nextPage` - number of next page
 
 ## Features to implement
 
 - Add more shelters
-- DB pagination
 
 ## Contributing
 
--- routers/api.js
+++ routers/api.js
@@ -4,27 +4,63 @@ const Dog = require('../models/dog');
 const router = new express.Router();
 
 // Sets all dogs list page url
-router.get('/api/all/:limit/:page', async (req, res) => {
-    const _limit = req.params.limit
-    const _page = req.params.page
+router.get('/api/all', async (req, res) => {
+    const _limit = req.query.limit
+    const _page = req.query.page
+    let data;
     try {
-        const data = await Dog.paginate({}, {limit: _limit, page: _page});
+        if (_limit && _page) {
+            data = await Dog.paginate({}, {
+                limit: _limit,
+                page: _page
+            });
+        } else {
+            data = await Dog.find({})
+        }
         res.send(data);
     } catch (error) {
         res.status(500).send()
     }
 });
 
-router.get('/api/shelter/:shelterName', async (req, res) => {
-    const _shelterName = req.params.shelterName
+router.get('/api/shelterNames', async (req, res) => {
     try {
-        const dog = await Dog.find({dataLocation: _shelterName})
+        data = await Dog.aggregate([{
+            $group: {
+                "_id": {
+                    location: '$location',
+                    dataLocation: '$dataLocation'
+                },
+            }
+        }]);
+        res.send(data)
+    } catch (error) {
+        res.status(500).send()
+    }
+})
 
-        if (!dog) {
+router.get('/api/shelter/:dataLocation', async (req, res) => {
+    const _dataLocation = req.params.dataLocation
+    const _limit = req.query.limit
+    const _page = req.query.page
+    let data;
+    try {
+        if (_limit && _page) {
+            data = await Dog.paginate({
+                dataLocation: _dataLocation
+            }, {
+                limit: _limit,
+                page: _page
+            });
+        } else {
+            data = await Dog.find({
+                dataLocation: _dataLocation
+            })
+        }
+        if (!data) {
             return res.status(404).send()
         }
-
-        res.send(dog)
+        res.send(data)
     } catch (error) {
         res.status(500).send()
         fs.appendFileSync('./logs/errors.log', `${new Date()}: Error: ${error} \n`);
@@ -34,13 +70,11 @@ router.get('/api/shelter/:shelterName', async (req, res) => {
 router.get('/api/id/:id', async (req, res) => {
     const _id = req.params.id
     try {
-        const dog = await Dog.findById(_id)
-
-        if (!dog) {
+        const data = await Dog.findById(_id)
+        if (!data) {
             return res.status(404).send()
         }
-
-        res.send(dog)
+        res.send(data)
     } catch (error) {
         res.status(500).send()
         fs.appendFileSync('./logs/errors.log', `${new Date()}: Error: ${error} \n`);
@@ -49,17 +83,32 @@ router.get('/api/id/:id', async (req, res) => {
 
 router.get('/api/name/:dogName', async (req, res) => {
     const _dogName = req.params.dogName
+    const _limit = req.query.limit
+    const _page = req.query.page
+    let data;
     try {
-        const dog = await Dog.find({"name": {
-            "$regex": _dogName.toLowerCase(),
-            "$options": "i"
-        }})
-
-        if (!dog) {
+        if (_limit && _page) {
+            data = await Dog.paginate({
+                "name": {
+                    "$regex": _dogName.toLowerCase(),
+                    "$options": "i"
+                }
+            }, {
+                limit: _limit,
+                page: _page
+            });
+        } else {
+            data = await Dog.find({
+                "name": {
+                    "$regex": _dogName.toLowerCase(),
+                    "$options": "i"
+                }
+            })
+        }
+        if (!data) {
             return res.status(404).send()
         }
-
-        res.send(dog)
+        res.send(data)
     } catch (error) {
         res.status(500).send()
         fs.appendFileSync('./logs/errors.log', `${new Date()}: Error: ${error} \n`);
@@ -70,12 +119,12 @@ router.get('/api/random/:count', async (req, res) => {
     const _count = req.params.count
     try {
         await Dog.findRandom({}, {}, {
-        limit: _count
-    }, function (error, results) {
-        if (!error) {
-            res.send(results)
-        }
-    })
+            limit: _count
+        }, function (error, results) {
+            if (!error) {
+                res.send(results)
+            }
+        })
     } catch (error) {
         res.status(500).send()
         fs.appendFileSync('./logs/errors.log', `${new Date()}: Error: ${error} \n`);
