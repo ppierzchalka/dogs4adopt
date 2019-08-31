e0d2db0de38f3073734f7ba81311204001592737
ppierzchalka <przemekpierzchalka@gmail.com>
2019-08-31 09:44:29 +0200

    change mongoose string

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,26 +1,26 @@
 const mongoose = require('mongoose');
 const fs = require('fs')
 
-const url = 'mongodb://mo1064_dogs4dopt:5Onw0yVtY3S7hwFqqdhU@91.185.191.145:27017/mo1064_dogs4dopt'; // Specify Your db connection here
+const url = 'mongodb://dfaadmin:AFluLAmOnKe1@ds042898.mlab.com:42898/dogs4dopt'; // Specify Your db connection here
 
 mongoose.connect(url, {
-        useNewUrlParser: true,
-        useCreateIndex: true
-    })
+  useNewUrlParser: true,
+  useCreateIndex: true,
+})
 
 // CONNECTION EVENTS
 // When successfully connected
-mongoose.connection.on('connected', function () {  
-    console.log('Mongoose connected');
-  }); 
-  
-  // If the connection throws an error
-  mongoose.connection.on('error',function (error) {  
-    console.log('Mongoose connection error: ' + error);
-    fs.appendFileSync('./logs/errors.log', `${new Date()}: Error: ${error} \n`);
-  }); 
-  
-  // When the connection is disconnected
-  mongoose.connection.on('disconnected', function () {  
-    console.log('Mongoose disconnected'); 
-  });
+mongoose.connection.on('connected', function () {
+  console.log('Mongoose connected');
+});
+
+// If the connection throws an error
+mongoose.connection.on('error', function (error) {
+  console.log('Mongoose connection error: ' + error);
+  fs.appendFileSync('./logs/errors.log', `${new Date()}: Error: ${error} \n`);
+});
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', function () {
+  console.log('Mongoose disconnected');
+});
